b3b443551f096d1a27fe5d91cb2f6ad7ff2eb962
Surya <surya.sk05@outlook.com>
2020-10-09 17:20:25 +0530

    Split sparkle lines into 2

--- 2DScreenSaver/ScreenSaver.c
+++ 2DScreenSaver/ScreenSaver.c
@@ -5,7 +5,7 @@ File: 			ScreenSaver.c
 
 Description:	A complete OpenGL program that draws a K on the screen.
 
-Author:			Surya Kashyap 
+Author:			Surya Kashyap
 
 *************************************************************************************/
 
@@ -31,22 +31,55 @@ GLfloat B1[2] = { 0.2,0.6 }, B2[2] = { 0.3,0.5 }, B3[2] = { -0.1,0.2 }, B4[2] = 
 //third polygon points
 GLfloat C1[2] = { 0.3, -0.1 }, C2[2] = { 0.2, -0.2 }, C3[2] = { -0.1,0.0 }, C4[2] = { -0.1, 0.2 };
 
+//first line of sparkle
 GLfloat P1[2] = { -0.9, 1.0 };
-GLfloat P2[2] = { -0.9, 0.9 };
+GLfloat P2[2] = { -0.9, 0.95 };
+GLfloat P3[2] = { -0.9, 0.95 };
+GLfloat P4[2] = { -0.9, 0.9 };
+
+//position to interpolate to
 GLfloat X1[2] = { -0.9, 1.0 };
-GLfloat X2[2] = { -0.9, 0.9 };
+GLfloat X2[2] = { -0.9, 0.95 };
+GLfloat X3[2] = { -0.9, 0.95 };
+GLfloat X4[2] = { -0.9, 0.9 };
+
+//second line of sparkle
 GLfloat Q1[2] = { -1.0, 0.95 };
-GLfloat Q2[2] = { -0.8, 0.95 };
+GLfloat Q2[2] = { -0.9, 0.95 };
+GLfloat Q3[2] = { -0.9, 0.95 };
+GLfloat Q4[2] = { -0.8, 0.95 };
+
+
+//
 GLfloat Y1[2] = { -1.0, 0.95 };
-GLfloat Y2[2] = { -0.8, 0.95 };
+GLfloat Y2[2] = { -0.9, 0.95 };
+GLfloat Y3[2] = { -0.9, 0.95 };
+GLfloat Y4[2] = { -0.8, 0.95 };
+
+//third line of sparkle
 GLfloat R1[2] = { -1.0, 1.0 };
+GLfloat R2[2] = { -0.9, 0.95 };
+GLfloat R3[2] = { -0.9,0.95 };
+GLfloat R4[2] = { -0.8, 0.9 };
+
+//third line of sparkle
 GLfloat V1[2] = { -1.0, 1.0 };
-GLfloat R2[2] = { -0.8, 0.9 };
-GLfloat V2[2] = { -0.8, 0.9 };
+GLfloat V2[2] = { -0.9, 0.95 };
+GLfloat V3[2] = { -0.9,0.95 };
+GLfloat V4[2] = { -0.8, 0.9 };
+
+//fourth line of sparkle
 GLfloat S1[2] = { -0.8, 1.0 };
+GLfloat S2[2] = { -0.9, 0.95 };
+GLfloat S3[2] = { -0.9, 0.95 };
+GLfloat S4[2] = { -1.0, 0.9 };
+
+//third line of sparkle
 GLfloat W1[2] = { -0.8, 1.0 };
-GLfloat S2[2] = { -1.0, 0.9 };
-GLfloat W2[2] = { -1.0, 0.9 };
+GLfloat W2[2] = { -0.9, 0.95 };
+GLfloat W3[2] = { -0.9, 0.95 };
+GLfloat W4[2] = { -1.0, 0.9 };
+
 
 //interpolation
 GLfloat sparkleInterp = 0.0;
@@ -124,54 +157,54 @@ void myIdle(void)
 	{
 		if (count == 0)
 		{
-			P1[0] = X1[0], P1[1] = X1[1], P2[0] = X2[0], P2[1] = X2[1];
-			Q1[0] = Y1[0], Q1[1] = Y1[1], Q2[0] = Y2[0], Q2[1] = Y2[1];
-			R1[0] = V1[0], R1[1] = V1[1], R2[0] = V2[0], R2[1] = V2[1];
-			S1[0] = W1[0], S1[1] = W1[1], S2[0] = W2[0], S2[1] = W2[1];
+			P1[0] = X1[0], P1[1] = X1[1], P2[0] = X2[0], P2[1] = X2[1], P3[0] = X3[0], P3[1] = X3[1], P4[0] = X4[0], P4[1] = X4[1];
+			Q1[0] = Y1[0], Q1[1] = Y1[1], Q2[0] = Y2[0], Q2[1] = Y2[1], Q3[0] = Y3[0], Q3[1] = Y3[1], Q4[0] = Y4[0], Q4[1] = Y4[1];
+			R1[0] = V1[0], R1[1] = V1[1], R2[0] = V2[0], R2[1] = V2[1], R3[0] = V3[0], R3[1] = V3[1], R4[0] = V4[0], R4[1] = V4[1];
+			S1[0] = W1[0], S1[1] = W1[1], S2[0] = W2[0], S2[1] = W2[1], S3[0] = W3[0], S3[1] = W3[1], S4[0] = W4[0], S4[1] = W4[1];
 
-			X1[0] = 0.9, X1[1] = 1.0, X2[0] = 0.9, X2[1] = 0.9;
-			Y1[0] = 1.0, Y1[1] = 0.95, Y2[0] = 0.8, Y2[1] = 0.95;
-			V1[0] = 1.0, V1[1] = 1.0, V2[0] = 0.8, V2[1] = 0.9;
-			W1[0] = 0.8, W1[1] = 1.0, W2[0] = 1.0, W2[1] = 0.9;
+			X1[0] = 0.9, X1[1] = 1.0, X2[0] = 0.9, X2[1] = 0.95, X3[0] = 0.9, X3[1] = 0.95, X4[0] = 0.9, X4[1] = 0.9;
+			Y1[0] = 1.0, Y1[1] = 0.95, Y2[0] = 0.9, Y2[1] = 0.95, Y3[0] = 0.9, Y3[1] = 0.95, Y4[0] = 0.8, Y4[1] = 0.95;
+			V1[0] = 1.0, V1[1] = 1.0, V2[0] = 0.9, V2[1] = 0.95, V3[0] = 0.9, V3[1] = 0.95, V4[0] = 0.8, V4[1] = 0.9;
+			W1[0] = 0.8, W1[1] = 1.0, W2[0] = 0.9, W2[1] = 0.95, W3[0] = 0.9, W3[1] = 0.95, W4[0] = 1.0, W4[1] = 0.9;
 			count++;
 		}
 		else if (count == 1)
 		{
-			P1[0] = X1[0], P1[1] = X1[1], P2[0] = X2[0], P2[1] = X2[1];
-			Q1[0] = Y1[0], Q1[1] = Y1[1], Q2[0] = Y2[0], Q2[1] = Y2[1];
-			R1[0] = V1[0], R1[1] = V1[1], R2[0] = V2[0], R2[1] = V2[1];
-			S1[0] = W1[0], S1[1] = W1[1], S2[0] = W2[0], S2[1] = W2[1];
+			P1[0] = X1[0], P1[1] = X1[1], P2[0] = X2[0], P2[1] = X2[1], P3[0] = X3[0], P3[1] = X3[1], P4[0] = X4[0], P4[1] = X4[1];
+			Q1[0] = Y1[0], Q1[1] = Y1[1], Q2[0] = Y2[0], Q2[1] = Y2[1], Q3[0] = Y3[0], Q3[1] = Y3[1], Q4[0] = Y4[0], Q4[1] = Y4[1];
+			R1[0] = V1[0], R1[1] = V1[1], R2[0] = V2[0], R2[1] = V2[1], R3[0] = V3[0], R3[1] = V3[1], R4[0] = V4[0], R4[1] = V4[1];
+			S1[0] = W1[0], S1[1] = W1[1], S2[0] = W2[0], S2[1] = W2[1], S3[0] = W3[0], S3[1] = W3[1], S4[0] = W4[0], S4[1] = W4[1];
 
-			X1[0] = -0.9, X1[1] = -0.8, X2[0] = -0.9, X2[1] = -0.7;
-			Y1[0] = -1.0, Y1[1] = -0.75, Y2[0] = -0.8, Y2[1] = -0.75;
-			V1[0] = -1.0, V1[1] = -0.8, V2[0] = -0.8, V2[1] = -0.7;
-			W1[0] = -0.8, W1[1] = -0.8, W2[0] = -1.0, W2[1] = -0.7;
+			X1[0] = -0.9, X1[1] = -0.8, X2[0] = -0.9, X2[1] = -0.85; X3[0] = -0.9, X3[1] = -0.85, X4[0] = -0.9, X4[1] = -0.7;
+			Y1[0] = -1.0, Y1[1] = -0.75, Y2[0] = -0.9, Y2[1] = -0.75, Y3[0] = -0.9, Y3[1] = -0.75, Y4[0] = -0.8, Y4[1] = -0.75;
+			V1[0] = -1.0, V1[1] = -0.8, V2[0] = -0.9, V2[1] = -0.75, V3[0] = -0.9, V3[1] = -0.75, V4[0] = -0.8, V4[1] = -0.7;
+			W1[0] = -0.8, W1[1] = -0.8, W2[0] = -0.9, W2[1] = -0.75, W3[0] = -0.9, W3[1] = -0.75, W4[0] = -1.0, W4[1] = -0.7;
 			count++;
 		}
 		else if (count == 2)
 		{
-			P1[0] = X1[0], P1[1] = X1[1], P2[0] = X2[0], P2[1] = X2[1];
-			Q1[0] = Y1[0], Q1[1] = Y1[1], Q2[0] = Y2[0], Q2[1] = Y2[1];
-			R1[0] = V1[0], R1[1] = V1[1], R2[0] = V2[0], R2[1] = V2[1];
-			S1[0] = W1[0], S1[1] = W1[1], S2[0] = W2[0], S2[1] = W2[1];
+			P1[0] = X1[0], P1[1] = X1[1], P2[0] = X2[0], P2[1] = X2[1], P3[0] = X3[0], P3[1] = X3[1], P4[0] = X4[0], P4[1] = X4[1];
+			Q1[0] = Y1[0], Q1[1] = Y1[1], Q2[0] = Y2[0], Q2[1] = Y2[1], Q3[0] = Y3[0], Q3[1] = Y3[1], Q4[0] = Y4[0], Q4[1] = Y4[1];
+			R1[0] = V1[0], R1[1] = V1[1], R2[0] = V2[0], R2[1] = V2[1], R3[0] = V3[0], R3[1] = V3[1], R4[0] = V4[0], R4[1] = V4[1];
+			S1[0] = W1[0], S1[1] = W1[1], S2[0] = W2[0], S2[1] = W2[1], S3[0] = W3[0], S3[1] = W3[1], S4[0] = W4[0], S4[1] = W4[1];
 
-			X1[0] = 0.9, X1[1] = -0.8, X2[0] = 0.9, X2[1] = -0.7;
-			Y1[0] = 1.0, Y1[1] = -0.75, Y2[0] = 0.8, Y2[1] = -0.75;
-			V1[0] = 1.0, V1[1] = -0.8, V2[0] = 0.8, V2[1] = -0.7;
-			W1[0] = 0.8, W1[1] = -0.8, W2[0] = 1.0, W2[1] = -0.7;
+			X1[0] = 0.9, X1[1] = -0.8, X2[0] = 0.9, X2[1] = -0.85; X3[0] = 0.9, X3[1] = -0.85, X4[0] = 0.9, X4[1] = -0.7;
+			Y1[0] = 1.0, Y1[1] = -0.75, Y2[0] = 0.9, Y2[1] = -0.75, Y3[0] = 0.9, Y3[1] = -0.75, Y4[0] = 0.8, Y4[1] = -0.75;
+			V1[0] = 1.0, V1[1] = -0.8, V2[0] = 0.9, V2[1] = -0.75, V3[0] = 0.9, V3[1] = -0.75, V4[0] = 0.8, V4[1] = -0.7;
+			W1[0] = 0.8, W1[1] = -0.8, W2[0] = 0.9, W2[1] = -0.75, W3[0] = 0.9, W3[1] = -0.75, W4[0] = 1.0, W4[1] = -0.7;
 			count++;
 		}
 		else if (count == 3)
 		{
-			P1[0] = X1[0], P1[1] = X1[1], P2[0] = X2[0], P2[1] = X2[1];
-			Q1[0] = Y1[0], Q1[1] = Y1[1], Q2[0] = Y2[0], Q2[1] = Y2[1];
-			R1[0] = V1[0], R1[1] = V1[1], R2[0] = V2[0], R2[1] = V2[1];
-			S1[0] = W1[0], S1[1] = W1[1], S2[0] = W2[0], S2[1] = W2[1];
+			P1[0] = X1[0], P1[1] = X1[1], P2[0] = X2[0], P2[1] = X2[1], P3[0] = X3[0], P3[1] = X3[1], P4[0] = X4[0], P4[1] = X4[1];
+			Q1[0] = Y1[0], Q1[1] = Y1[1], Q2[0] = Y2[0], Q2[1] = Y2[1], Q3[0] = Y3[0], Q3[1] = Y3[1], Q4[0] = Y4[0], Q4[1] = Y4[1];
+			R1[0] = V1[0], R1[1] = V1[1], R2[0] = V2[0], R2[1] = V2[1], R3[0] = V3[0], R3[1] = V3[1], R4[0] = V4[0], R4[1] = V4[1];
+			S1[0] = W1[0], S1[1] = W1[1], S2[0] = W2[0], S2[1] = W2[1], S3[0] = W3[0], S3[1] = W3[1], S4[0] = W4[0], S4[1] = W4[1];
 
-			X1[0] = -0.9, X1[1] = 1.0, X2[0] = -0.9, X2[1] = 0.9;
-			Y1[0] = -1.0, Y1[1] = 0.95, Y2[0] = -0.8, Y2[1] = 0.95;
-			V1[0] = -1.0, V1[1] = 1.0, V2[0] = -0.8, V2[1] = 0.9;
-			W1[0] = -0.8, W1[1] = 1.0, W2[0] = -1.0, W2[1] = 0.9;
+			X1[0] = -0.9, X1[1] = 1.0, X2[0] = -0.9, X2[1] = 0.95, X3[0] = -0.9, X3[1] = 0.95, X4[0] = -0.9, X4[1] = 0.9;
+			Y1[0] = -1.0, Y1[1] = 0.95, Y2[0] = -0.9, Y2[1] = 0.95, Y3[0] = -0.9, Y3[1] = 0.95, Y4[0] = -0.8, Y4[1] = 0.95;
+			V1[0] = -1.0, V1[1] = 1.0, V2[0] = -0.9, V2[1] = 0.95, V3[0] = -0.9, V3[1] = 0.95, V4[0] = -0.8, V4[1] = 0.9;
+			W1[0] = -0.8, W1[1] = 1.0, W2[0] = -0.9, W2[1] = 0.95, W3[0] = -0.9, W3[1] = 0.95, W4[0] = -1.0, W4[1] = 0.9;
 
 			count = 0;
 		}
@@ -230,15 +263,23 @@ void display(void)
 	glColor3f(1.0, 1.0, 1.0);
 	glVertex2f((1 - sparkleInterp) * P1[0] + sparkleInterp * X1[0], (1 - sparkleInterp) * P1[1] + sparkleInterp * X1[1]);
 	glVertex2f((1 - sparkleInterp) * P2[0] + sparkleInterp * X2[0], (1 - sparkleInterp) * P2[1] + sparkleInterp * X2[1]);
+	glVertex2f((1 - sparkleInterp) * P3[0] + sparkleInterp * X3[0], (1 - sparkleInterp) * P3[1] + sparkleInterp * X3[1]);
+	glVertex2f((1 - sparkleInterp) * P4[0] + sparkleInterp * X4[0], (1 - sparkleInterp) * P4[1] + sparkleInterp * X4[1]);
 
 	glVertex2f((1 - sparkleInterp) * Q1[0] + sparkleInterp * Y1[0], (1 - sparkleInterp) * Q1[1] + sparkleInterp * Y1[1]);
 	glVertex2f((1 - sparkleInterp) * Q2[0] + sparkleInterp * Y2[0], (1 - sparkleInterp) * Q2[1] + sparkleInterp * Y2[1]);
+	glVertex2f((1 - sparkleInterp) * Q3[0] + sparkleInterp * Y3[0], (1 - sparkleInterp) * Q3[1] + sparkleInterp * Y3[1]);
+	glVertex2f((1 - sparkleInterp) * Q4[0] + sparkleInterp * Y4[0], (1 - sparkleInterp) * Q4[1] + sparkleInterp * Y4[1]);
 
 	glVertex2f((1 - sparkleInterp) * R1[0] + sparkleInterp * V1[0], (1 - sparkleInterp) * R1[1] + sparkleInterp * V1[1]);
 	glVertex2f((1 - sparkleInterp) * R2[0] + sparkleInterp * V2[0], (1 - sparkleInterp) * R2[1] + sparkleInterp * V2[1]);
+	glVertex2f((1 - sparkleInterp) * R3[0] + sparkleInterp * V3[0], (1 - sparkleInterp) * R3[1] + sparkleInterp * V3[1]);
+	glVertex2f((1 - sparkleInterp) * R4[0] + sparkleInterp * V4[0], (1 - sparkleInterp) * R4[1] + sparkleInterp * V4[1]);
 
 	glVertex2f((1 - sparkleInterp) * S1[0] + sparkleInterp * W1[0], (1 - sparkleInterp) * S1[1] + sparkleInterp * W1[1]);
 	glVertex2f((1 - sparkleInterp) * S2[0] + sparkleInterp * W2[0], (1 - sparkleInterp) * S2[1] + sparkleInterp * W2[1]);
+	glVertex2f((1 - sparkleInterp) * S3[0] + sparkleInterp * W3[0], (1 - sparkleInterp) * S3[1] + sparkleInterp * W3[1]);
+	glVertex2f((1 - sparkleInterp) * S4[0] + sparkleInterp * W4[0], (1 - sparkleInterp) * S4[1] + sparkleInterp * W4[1]);
 	glEnd();
 
 	//draw the bottom bar
